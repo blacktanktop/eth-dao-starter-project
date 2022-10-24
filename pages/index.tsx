d18fa3f7f423e7d7e49e061eac49a738bc1f61e0
blacktanktop <blacktanktopme@gmail.com>
2022-10-24 18:34:47 +0900

    Update Initialize SDK

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,39 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+// 接続中のネットワークを取得するため useNetwork を新たにインポートします。
+import { ConnectWallet, ChainId, useNetwork, useAddress } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          {/* 自分の作成する DAO 名に変更してください */}
-          Welcome to blacktanktop DAO!!
-        </h1>
-        <p></p>
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-      </main>
-    </div>
-  );
+  const address = useAddress();
+  const [network, switchNetwork] = useNetwork();
+
+  if (address && network && network?.data?.chain?.id !== ChainId.Goerli) {
+    console.log("wallet address: ", address);
+    console.log("network: ", network?.data?.chain?.id);
+
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Goerli に切り替えてください⚠️</h1>
+          <p>この dApp は Goerli テストネットのみで動作します。</p>
+          <p>ウォレットから接続中のネットワークを切り替えてください。</p>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to blacktanktop Collective !!
+          </h1>
+          <div className={styles.connect}>
+            <ConnectWallet />
+          </div>
+        </main>
+      </div>
+    );
+  }
 };
 
 export default Home;
